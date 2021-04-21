337cebc5dd157b76a63e7cb052656b0012757e25
JudgementH <446995859@qq.com>
2021-04-21 09:08:15 +0800

    update

--- data/rating_dataset.py
+++ data/rating_dataset.py
@@ -6,13 +6,17 @@
 
 __author__ = 'Judgement'
 
+import numpy as np
+
 from torch.utils.data import Dataset
+from preprocess import read_csv
 
 
-class LabeledDataSet(Dataset):
+class RatingDataSet(Dataset):
 
     def __init__(self, data_path):
         self.data_path = data_path
+        self.rating_list = read_csv(data_path)
 
     def __len__(self):
         pass
@@ -23,3 +27,6 @@ class LabeledDataSet(Dataset):
     @staticmethod
     def collate_fn(batch):
         pass
+
+if __name__ == '__main__':
+    dataset = LabeledDataSet()
